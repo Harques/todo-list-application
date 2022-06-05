920c99536549425e493525b93884bd254fde852c
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2022-06-05 16:05:02 +0300

    Gateway eklendi.

---
+++ api-gateway/src/main/java/com/microservices/api/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.microservices.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	@Autowired
+	private GatewayProperties props;
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
